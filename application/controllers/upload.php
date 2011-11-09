d49a9688cea0127561bb0dfd33126b90f14baa2e
weatheredwatcher <weatheredwatcher@gmail.com>
2011-11-09 10:31:23 -0500

    added the upload controller and views

---
+++ application/controllers/upload.php
@@ -0,0 +1,40 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Upload extends CI_Controller {
+
+	function __construct()
+	{
+		parent::__construct();
+	}
+		
+	function index()
+	{
+		$this->load->view('upload/upload_form');
+	}
+	
+	function do_upload()
+		{
+			$config['upload_path'] = './uploads/';
+			$config['allowed_types'] = 'gif|jpg|png|mp4';  
+			$config['max_size']	= '400000';
+			$config['remove_spaces'] = TRUE;
+			//$config['max_width']  = '1024';
+			//$config['max_height']  = '768';
+
+			$this->upload->initialize($config);
+
+			if ( ! $this->upload->do_upload())
+			{
+				$error = array('error' => $this->upload->display_errors());
+
+				$this->load->view('upload_form', $error);
+			}
+			else
+			{
+				$data = array('upload_data' => $this->upload->data());
+
+				$this->load->view('upload/upload_success', $data);
+			}
+		}
+	
+}
